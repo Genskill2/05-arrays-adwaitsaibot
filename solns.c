e072476fa8052e6c6cab68aca0a1aa02b3e804ca
adwaitsaibot <85330213+adwaitsaibot@users.noreply.github.com>
2021-06-19 11:34:57 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,92 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+int max(int x[],int k)
+{
+  int t,i;
+  t=x[0];
+  for(i=1;i<k;i++)
+  {
+    if(x[i]>t)
+      t=x[i];
+  }
+  return(t);
+}
+
+int min(int a[],int n)
+{
+  int min=a[0];
+  for(int i=0;i<n;i++)
+  {
+    if(a[i]<min)
+      min=a[i];
+    else
+      continue;
+  }
+  return min;
+}
+
+ float average(int a[],int n)
+ {
+   int i;
+   float sum=0;
+   for(i=0;i<n;i++)
+   {
+     sum=sum+a[i];
+   }
+   return(sum/n);
+ }
+
+ int mode(int a[],int n)
+ {
+   int maxvalue=0,maxcount=0,i,j;
+   for(i=0;i<n;++i)
+   {
+     int count=0;
+     for(j=0;j<n;++j)
+     {
+       if(a[j]==a[i])
+         ++count;
+     }
+     if(count>maxcount)
+     {
+       maxcount=count;
+       maxvalue=a[i];
+     }
+   }
+   return(maxvalue);
+ }
+
+int factors(int num,int arr[])
+{
+  int cpy=num;
+  int cnt=0;
+  int cnt1=0;
+  for(int i=2;i<num/2;i++)
+  {
+    if(num%i==0)
+    {
+      for(int j=2;j<=i;j++)
+      {
+        if(i%j==0)
+          cnt+=1;
+      }
+      if(cnt==1)
+      {
+        while(cpy%i==0)
+        {
+          arr[cnt1]=i;
+          cpy/=i;
+          cnt1+=1;
+        }
+        cpy=num;
+      }
+        cnt=0;
+      }
+      else
+        continue;
+    }
+    return cnt1;
+}
+  
+      
+  
